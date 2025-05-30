f0ad4a457621a51435d65cda911ea3ed3a01bdc8
nellymiteva1 <154827899+nellymiteva1@users.noreply.github.com>
2025-05-30 17:29:57 +0300

    Delete course-work/Implementations/LostAndFoundProject/lost-and-found-frontend/src/PrivateRoute.js

--- course-work/Implementations/LostAndFoundProject/lost-and-found-frontend/src/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { isAuthenticated } from '../utils/auth';
-
-export default function PrivateRoute({ children }) {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
-}
