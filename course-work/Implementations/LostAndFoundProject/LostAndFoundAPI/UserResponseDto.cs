81b6be5f032fc2b620969e3590c9eb7f2cf61f90
nellymiteva1 <154827899+nellymiteva1@users.noreply.github.com>
2025-05-30 17:50:45 +0300

    Delete course-work/Implementations/LostAndFoundProject/LostAndFoundAPI/UserResponseDto.cs

--- course-work/Implementations/LostAndFoundProject/LostAndFoundAPI/UserResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LostAndFoundAPI.DTOs
-{
-    public class UserResponseDto
-    {
-        public int Id { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-    }
-}
